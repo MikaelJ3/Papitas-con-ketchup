e1d99a11262c7d8baf0f832fa3f8a1f8266ea382
MikaelJ3 <ultramikej@hotmail.com>
2018-01-31 21:28:18 -0400

    update db config to heroku db deleted port

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,7 +1,7 @@
 #Databse configuration information
 
 pg_config = {
-    'host': 'ec2-184-73-201-79.compute-1.amazonaws.com:5432',
+    'host': 'ec2-184-73-201-79.compute-1.amazonaws.com',
     'user' : 'gebchnljikxhqz',
     'passwd' : '7e26185cd44b3bcf843c82376dc3a467edd307aa3eda00d63159b3d812f83625',
     'dbname' : 'dc9jtumdafasnt'
