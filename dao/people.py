caa09c185160df61093cc1adf978b0a3c85f31c2
MikaelJ3 <ultramikej@hotmail.com>
2018-02-02 14:13:05 -0400

    Testing getPINByFirstName 2.2

--- dao/people.py
+++ dao/people.py
@@ -328,96 +328,96 @@ class peopledao:
         return result
 
         ### A D M I N I S T R A T O R S ####################################################################################
-        def getAllAdmin(self):
-            cursor = self.conn.cursor()
-            query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
-                    "district " \
-                    "from admins natural inner join addresses;"
-            cursor.execute(query)
-            result = []
-            for row in cursor:
-                result.append(row)
-            return result
+    def getAllAdmin(self):
+        cursor = self.conn.cursor()
+        query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
+                "district " \
+                "from admins natural inner join addresses;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
 
-        def GetADMINByID(self, ad_id):
-            cursor = self.conn.cursor()
-            query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
-                    "district " \
-                    "from admins natural inner join addresses" \
-                    "where ad_id = %s;"
-            cursor.execute(query, (ad_id,))
-            result = cursor.fetchone()
-            return result
+    def GetADMINByID(self, ad_id):
+        cursor = self.conn.cursor()
+        query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
+                "district " \
+                "from admin natural inner join addresses" \
+                "where ad_id = %s;"
+        cursor.execute(query, (ad_id,))
+        result = cursor.fetchone()
+        return result
 
-        def GetADMINByFNAME(self, ad_fname):
-            cursor = self.conn.cursor()
-            query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
-                    "district " \
-                    "from admins natural inner join addresses" \
-                    "where ad_fname = %s;"
-            cursor.execute(query, (ad_fname,))
-            result = cursor.fetchone()
-            for row in cursor:
-                result.append(row)
-            return result
+    def GetADMINByFNAME(self, ad_fname):
+        cursor = self.conn.cursor()
+        query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
+                "district " \
+                "from admin natural inner join addresses" \
+                "where ad_fname = %s;"
+        cursor.execute(query, (ad_fname,))
+        result = cursor.fetchone()
+        for row in cursor:
+            result.append(row)
+        return result
 
-        def GetADMINByPHONE(self, ad_phone):
-            cursor = self.conn.cursor()
-            query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
-                    "district " \
-                    "from admins natural inner join addresses" \
-                    "where ad_phone = %s;"
-            cursor.execute(query, (ad_phone,))
-            result = cursor.fetchone()
-            return result
+    def GetADMINByPHONE(self, ad_phone):
+        cursor = self.conn.cursor()
+        query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
+                "district " \
+                "from admin natural inner join addresses" \
+                "where ad_phone = %s;"
+        cursor.execute(query, (ad_phone,))
+        result = cursor.fetchone()
+        return result
 
-        def GetADMINByCITY(self, city):
-            cursor = self.conn.cursor()
-            query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
-                    "district " \
-                    "from admins natural inner join addresses" \
-                    "where city = %s;"
-            cursor.execute(query, (city,))
-            result = cursor.fetchone()
-            for row in cursor:
-                result.append(row)
-            return result
+    def GetADMINByCITY(self, city):
+        cursor = self.conn.cursor()
+        query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
+                "district " \
+                "from admin natural inner join addresses" \
+                "where city = %s;"
+        cursor.execute(query, (city,))
+        result = cursor.fetchone()
+        for row in cursor:
+            result.append(row)
+        return result
 
-        def GetADMINByCOUNTRY(self, country):
-            cursor = self.conn.cursor()
-            query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
-                    "district " \
-                    "from admins natural inner join addresses" \
-                    "where country = %s;"
-            cursor.execute(query, (country,))
-            result = cursor.fetchone()
-            for row in cursor:
-                result.append(row)
-            return result
+    def GetADMINByCOUNTRY(self, country):
+        cursor = self.conn.cursor()
+        query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
+                "district " \
+                "from admin natural inner join addresses" \
+                "where country = %s;"
+        cursor.execute(query, (country,))
+        result = cursor.fetchone()
+        for row in cursor:
+            result.append(row)
+        return result
 
-        def GetADMINByDISTRICT(self, district):
-            cursor = self.conn.cursor()
-            query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
-                    "district " \
-                    "from admins natural inner join addresses" \
-                    "where district = %s;"
-            cursor.execute(query, (district,))
-            result = cursor.fetchone()
-            for row in cursor:
-                result.append(row)
-            return result
+    def GetADMINByDISTRICT(self, district):
+        cursor = self.conn.cursor()
+        query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
+                "district " \
+                "from admin natural inner join addresses" \
+                "where district = %s;"
+        cursor.execute(query, (district,))
+        result = cursor.fetchone()
+        for row in cursor:
+            result.append(row)
+        return result
 
-        def GeADMINByFULLNAME(self, ad_fname, ad_lname):
-            cursor = self.conn.cursor()
-            query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
-                    "district " \
-                    "from admins natural inner join addresses" \
-                    "where ad_fname = %s and ad_lname = %s;"
-            cursor.execute(query, (ad_fname, ad_lname,))
-            result = cursor.fetchone()
-            for row in cursor:
-                result.append(row)
-            return result
+    def GeADMINByFULLNAME(self, ad_fname, ad_lname):
+        cursor = self.conn.cursor()
+        query = "select ad_id, ad_fname, ad_lname, a_id, ad_phone, addressid, addressline1, city, zipcode, country, " \
+                "district " \
+                "from admin natural inner join addresses" \
+                "where ad_fname = %s and ad_lname = %s;"
+        cursor.execute(query, (ad_fname, ad_lname,))
+        result = cursor.fetchone()
+        for row in cursor:
+            result.append(row)
+        return result
 
 
     ''' not specified in phase 2 specs'''
