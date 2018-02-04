3f09587cdab30c9c8591eef1b6efab69fa86d6e8
MikaelJ3 <ultramikej@hotmail.com>
2018-02-04 18:37:25 -0400

    chucha

--- handler/people.py
+++ handler/people.py
@@ -600,7 +600,7 @@ class peopleHandler:
         result_list = []
         pin = dao.get_pin(pin_id)
         for row in pin:
-            result = self.build_pin_dict(row)
+            result = self.build_pinINS_dict(row)
             result_list.append(result)
         return jsonify(PIN=result_list)
 
